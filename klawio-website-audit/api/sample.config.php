6dc1b6aacb5b07baa8d381c71359fea646ff6291
M. Hakan TATLICI <hakantatlici@hotmail.com>
2025-11-26 20:55:56 -0600

    Create sample.config.php

---
+++ klawio-website-audit/api/sample.config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Copy this file to config.php and fill in your own credentials.
+
+return [
+    "API_KEY"     => "YOUR_GOOGLE_PAGESPEED_API_KEY",
+    "SMTP_FROM"   => "your-email@example.com",
+    "SMTP_TO_YOU" => "your-email@example.com"
+];
